7195fa980891dfb35b3661730f2641e9ff766441
psm2360440 <psm2360440@gmail.com>
2023-08-05 17:14:43 +0900

    jstl_pom.xml추가 및 center나누기 테스트

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,12 @@ public class MainController {
 	
 	@RequestMapping("/")
 	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/services")
+	public String services(Model model) throws Exception {
+		model.addAttribute("center", "services");
 		return "index";
 	}
 }
