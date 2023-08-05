6fddccba216801aae7cca1946f2b7c1287f2be6d
David Byun <byundavid@naver.com>
2023-08-05 17:16:36 +0900

    posts 전체 리스트 뒷단(화면단 개발 필요)

--- src/main/java/com/example/demo/mapper/PostMapper.java
+++ src/main/java/com/example/demo/mapper/PostMapper.java
@@ -1,8 +1,9 @@
 package com.example.demo.mapper;
 
+import com.example.demo.dto.Post;
 import com.example.demo.frame.DemoMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface PostMapper extends DemoMapper {
+public interface PostMapper extends DemoMapper<Integer, Post> {
 }
